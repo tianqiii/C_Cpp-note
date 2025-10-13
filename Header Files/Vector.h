9c8d45af59297c86dcde5bfed360d722af5f7484
Bao_nini <102462532+tianqiii@users.noreply.github.com>
2025-10-13 22:43:57 +0800

    add some changes

--- Header Files/Vector.h
+++ Header Files/Vector.h
@@ -2,6 +2,8 @@
 
 #include <cstddef>
 #include <mutex>
+#include <new>
+#include <utility>
 
 template <typename T> class Vector
 {
@@ -12,21 +14,28 @@ private:
     size_t m_Capacity = 0;
 
 private:
-    void ReAlloc(size_t newCapacity)
+    void ReAlloc(size_t newCapacity) // 应该只分配空间而不构造
     {
         // 1. 分配一块额外的内存空间
         // 2. 将已有的内存复制到新的内存空间
         // 3. 删除旧空间
 
+        // 注意，这里使用了new 分配了一个内存空间并且使用构造函数
+        // 后面的 delete 可能会导致双重析构错误,可以将分配空间和构造分开
         T *newBlock = new T[newCapacity];
+        // T *newBlock = (T *)::operator new(newCapacity * sizeof(T));
 
         if (newCapacity < m_Size)
             m_Size = newCapacity;
 
         for (size_t i = 0; i < m_Size; i++)
-            newBlock[i] = m_Data[i];
+            newBlock[i] = std::move(m_Data[i]);
+
+        // for (size_t i = 0; i < m_Size; i++)
+        //     m_Data[i].~T();
 
         delete[] m_Data;
+        // ::operator delete(m_Data, m_Capacity * sizeof(T));
         m_Data = newBlock;
         m_Capacity = newCapacity;
     }
@@ -38,6 +47,12 @@ public:
         ReAlloc(2);
     }
 
+    ~Vector()
+    {
+        Clear();
+        delete[] m_Data;
+    }
+
     void PushBack(const T &value)
     {
         if (m_Size >= m_Capacity)
@@ -47,6 +62,40 @@ public:
 
         m_Data[m_Size] = value;
         m_Size++;
+    }
+    // 可变参数模板
+    template <typename... Args> T &EmplaceBack(Args &&...args)
+    {
+        if (m_Size >= m_Capacity)
+            ReAlloc(m_Capacity + m_Capacity / 2);
+
+        // 放在括号后表示展开参数包
+        // 完美转发：将参数转发回原来的类别，确保调用左值和右值构造函数
+
+        // m_Data[m_Size] = T(std::forward<Args>(args)...);
+        // return m_Data[m_Size++];
+
+        // 原地构造 new(address) T(...) 在address上直接使用T的构造函数构造
+        // 高效，避免拷贝，析构时必须手动调用T::~T(),否则会导致内存泄露
+        new (&m_Data[m_Size]) T(std::forward<Args>(args)...);
+        return m_Data[m_Size++];
+    }
+
+    void PopBack()
+    {
+        if (m_Size > 0)
+        {
+            m_Size--;
+            m_Data[m_Size].~T();
+        }
+    }
+
+    void Clear()
+    {
+        for (size_t i = 0; i < m_Size; i++)
+            m_Data[i].~T();
+
+        m_Size = 0;
     }
 
     const T &operator[](size_t index) const
--- src/my_vector.cpp
+++ src/my_vector.cpp
@@ -15,30 +15,52 @@ template <typename T> void PrintVector(const Vector<T> &vector)
 struct Vector3
 {
     float x = 0.0f, y = 0.0f, z = 0.0f;
+    // int  *m_MemoryBlock = nullptr;
 
     Vector3() {}
 
-    Vector3(float scalar) : x(scalar), y(scalar), z(scalar) {}
+    Vector3(float scalar) : x(scalar), y(scalar), z(scalar)
+    {
+        // m_MemoryBlock = new int[5];
+    }
     Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 
-    Vector3(const Vector3 &other) : x(other.x), y(other.y), z(other.z)
+    Vector3(const Vector3 &other) // = delete;
+        : x(other.x), y(other.y), z(other.z)
     {
         std::cout << "Copied! \n";
     }
 
     Vector3(Vector3 &&other) : x(other.x), y(other.y), z(other.z)
     {
+        // m_MemoryBlock = other.m_MemoryBlock;
+        // other.m_MemoryBlock = nullptr;
         std::cout << "Moved! \n";
     }
-
-    Vector3 &operator=(const Vector3 &value)
+    // 拷贝赋值
+    Vector3 &operator=(const Vector3 &other) //= delete;
     {
         std::cout << "Copy assigned!\n";
-        if (this != &value)
+        if (this != &other)
         {
-            x = value.x;
-            y = value.y;
-            z = value.z;
+            x = other.x;
+            y = other.y;
+            z = other.z;
+        }
+        return *this;
+    }
+    // 移动赋值
+    Vector3 &operator=(Vector3 &&other) noexcept
+    {
+        // m_MemoryBlock = other.m_MemoryBlock;
+        // other.m_MemoryBlock = nullptr;
+
+        std::cout << "Move assigned!\n";
+        if (this != &other)
+        {
+            x = other.x;
+            y = other.y;
+            z = other.z;
         }
         return *this;
     }
@@ -46,13 +68,15 @@ struct Vector3
     ~Vector3()
     {
         std::cout << "Destroyed! \n";
+        // delete[] m_MemoryBlock;
     }
 };
-// 特化模板
+// 特化模板 需要传递特定的模板参数而不是T
 template <> void PrintVector(const Vector<Vector3> &vector)
 {
     for (size_t i = 0; i < vector.Size(); i++)
         std::cout << vector[i].x << "," << vector[i].y << "," << vector[i].z << "\n";
+    std::cout << std::string(30, '-') << "\n";
 }
 
 int main()
@@ -64,11 +88,27 @@ int main()
     Vector<std::string> vector;
     vector.PushBack("ztq");
     vector.PushBack("C++");
-    // vector.EmplaceBack("vector");
-    Vector<Vector3> vec;
-    vec.PushBack(Vector3(1.0f));
-    vec.PushBack(Vector3(2, 3, 4));
-    vec.PushBack(Vector3());
 
+    vector.EmplaceBack("vector");
+    vector.EmplaceBack();
     PrintVector(vector);
+
+    vector.PopBack();
+    vector.PopBack();
+    PrintVector(vector);
+
+    Vector<Vector3> vec;
+    vec.EmplaceBack(Vector3(1.0f));
+    /* 输出：
+    Moved! -> 右值临时对象移动到内部
+    Move assigned! -> Vector内部数组位置赋值
+    Destroyed! -> 临时对象销毁
+    Des
+    */
+    vec.EmplaceBack(Vector3(2, 3, 4));
+    vec.EmplaceBack(Vector3());
+    PrintVector(vec);
+
+    vec.Clear();
+    PrintVector(vec);
 }
